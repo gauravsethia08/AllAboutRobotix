42b248f3363b13a9eb3f2f86aa089ea121ef3ac4
Gaurav Sethia <gaurav.sethia08@gmail.com>
2020-11-11 02:43:00 +0530

    User Auth

--- account/models.py
+++ account/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django import forms
 
 # Create your models here.
+class studentsManager(BaseUserManager):
+
+  def _create_user(self, email, password, **extra_fields):
+    if not email:
+        raise ValueError('Users must have an email address')
+    
+    email = self.normalize_email(email)
+    user = self.model(
+        email=email,
+        **extra_fields
+    )
+    user.set_password(password)
+    user.save(using=self._db)
+    return user
+
+
+
+class students(models.Model):
+    name = models.CharField(max_length=20)
+    email = models.EmailField()
+    password = models.CharField(max_length=16, blank=False)
+    reg_date = models.DateTimeField(auto_now_add=True)
+    num_course_enrolled = models.IntegerField(default=0)
+    num_course_completed = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.email
--- account/views.py
+++ account/views.py
@@ -1,6 +1,11 @@
 from django.contrib import messages
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
+from .models import students
+import bcrypt
+from django.contrib.auth.hashers import make_password
+from django.contrib.auth.models import auth
+
 
 # Create your views here.
 def login(request):
@@ -13,7 +18,10 @@ def login(request):
         email = request.POST['email']
         password = request.POST['password']
 
-
+        user = auth.authenticate(email = email, password = password)
+        
+        messages.error(request, "Login Successful")
+        return HttpResponseRedirect('/')
 
     #Load the Login Page if GET method is used
     else:
@@ -27,15 +35,30 @@ def register(request):
         #Fetching data from the HTML page
         name = request.POST['name']
         email = request.POST['email']
-        password = request.POST['password']
+        password1 = request.POST['password']
         password2 = request.POST['password2']
 
-        #Checking is Login password and repeated login password are same
-        if password == password2:
-           pass
+        if email != str(students.objects.filter(email=email).first()):
+            #Checking is Login password and repeated login password are same
+            if password1 == password2:
+
+                password = make_password(password1)
+
+                user = students(email=email, name=name, password=password)
+                user.save()
+
+                messages.error(request, "Registration Successfull")
+                return HttpResponseRedirect('login')
+            
+
+            else:
+                #Raising an exception
+                messages.error(request, "Password is not matching")
+                return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
         else:
             #Raising an exception
-            messages.error(request, "Password is not matching")
+            messages.error(request, "Email Already Registered")
             return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
     #Load the Login Page if GET method is used
